03087f38bbec57689d02f4eb28c25cd368d58863
quntico <63abhikumar@gmail.com>
2020-10-27 19:51:05 +0530

    learned something new

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,22 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button Button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button = findViewById(R.id.button);
-        Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Button.setText("clicked");
-            }
-        });
+
+
+
+    }
+
+    public void onclick (View view) {
+        TextView txtHello = findViewById(R.id.textView);
+        EditText edtTextname = findViewById(R.id.edtTextname);
+        txtHello.setText("Hello " + edtTextname.getText().toString());
     }
 }
